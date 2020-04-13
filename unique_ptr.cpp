f5ecdb80315355272b3c16bdecf9a1ef64016e9b
Cameron Durham <camerondurham@users.noreply.github.com>
2020-04-13 02:36:55 -0500

    Add minimal examples and notes

---
+++ unique_ptr.cpp
@@ -0,0 +1,16 @@
+#include<memory>
+int main()
+{
+
+    auto num = std::make_unique<int>(42);
+    std::unique_ptr<int> thief;
+    // thief = num; // error: use of deleted function
+
+    auto owner = std::make_unique<std::string>("Foo");
+    std::unique_ptr<std::string> newOwner;
+    std::unique_ptr<std::string> newOwner2(std::move(owner));
+    // newOwner = owner; // error!
+
+
+
+}
